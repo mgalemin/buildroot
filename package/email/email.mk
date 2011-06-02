53f0270dd472419f692e92c5fb2f5680cfaf8f41
Max Galemin <maksim.galemin@gmail.com>
2011-06-03 08:33:55 +1000

    Issue 8: fixed invalid dependency for email (openssl).

--- package/email/email.mk
+++ package/email/email.mk
@@ -8,7 +8,7 @@ EMAIL_SITE = http://www.cleancode.org/downloads/email
 
 ifeq ($(BR2_PACKAGE_OPENSSL),y)
 EMAIL_CONF_OPT += --with-ssl
-MONIT_DEPENDENCIES += openssl
+EMAIL_DEPENDENCIES += openssl
 endif
 
 $(eval $(call AUTOTARGETS,package,email))
