d8555710c593d5d71907c79ed07ec2a20bd4f244
Max Galemin <maksim.galemin@gmail.com>
2012-02-29 22:20:46 +1100

    Issue 20: Additonal packages for Buildroot ver. 2011.11
    1. Changed name for the U-Boot first stage bootloader in configs from MLO to SPL;
    2. Updated Linux kernel version to the 3.2.8;
    3. Added patch for fixing invalid ISP CCDC pipeline in Linux kernel.

--- boot/uboot/uboot.mk
+++ boot/uboot/uboot.mk
@@ -33,8 +33,8 @@ else
 UBOOT_BIN          = u-boot.bin
 endif
 
-ifeq ($(BR2_TARGET_UBOOT_MLO),y)
-UBOOT_MLO = MLO
+ifeq ($(BR2_TARGET_UBOOT_SPL),y)
+UBOOT_SPL = MLO
 endif
 
 ifeq ($(BR2_TARGET_UBOOT_UENV_PARAMETERS),y)
@@ -97,8 +97,8 @@ endef
 
 define UBOOT_INSTALL_IMAGES_CMDS
 	cp -dpf $(@D)/$(UBOOT_BIN) $(BINARIES_DIR)/
-	if [ $(BR2_TARGET_UBOOT_MLO) = y ]; then \
-		cp -dpf $(@D)/$(UBOOT_MLO) $(BINARIES_DIR)/; \
+	if [ $(BR2_TARGET_UBOOT_SPL) = y ]; then \
+		cp -dpf $(@D)/$(UBOOT_SPL) $(BINARIES_DIR)/; \
 	fi
 	if [ $(BR2_TARGET_UBOOT_UENV_PARAMETERS) = y ]; then \
 		echo "mpurate="$(BR2_TARGET_UBOOT_UENV_MPURATE) >> $(BINARIES_DIR)/$(UBOOT_UENV); \
